b523c3f1808b2595aab8c418b1919eb665bdf23e
Juliana Zgryzek <j.zgryzek@bynary.io>
2021-11-10 14:04:33 +0100

    tests of app.component

--- apps/todos/src/app/app.component.spec.ts
+++ apps/todos/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { By } from '@angular/platform-browser';
+import { LoggingService } from './logging.service';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -15,27 +16,85 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
   });
 
   describe('Logic', () => {
     describe('click button', () => {
-      it('should click', () => {
 
+      let componentClickedSpy: jest.SpyInstance;
+
+      beforeEach(() => {
+        componentClickedSpy = jest.spyOn(component, 'clicked');
+        jest.useFakeTimers();
+        jest.spyOn(global, 'setTimeout');
+        //componentClickedSpy.mockImplementation(() => console.log('hello'));
+      })
+
+      afterEach(() => {
+        componentClickedSpy.mockClear();
+      })
+
+      it('should click',  () => {
+        const button = fixture.debugElement.nativeElement.querySelector('button');
+
+        button.click();
+
+        expect(component.clicked).toHaveBeenCalled();
+      });
+
+      it('should change message', () => {
+        const button = fixture.debugElement.nativeElement.querySelector('button');
+
+        button.click();
+
+        expect(component.message).toEqual('The light is On');
+
+        button.click();
+
+        expect(component.message).toEqual('The light is Off');
+      })
+
+      it('test counter', fakeAsync(() => {
+        component.countClick();
+
+        tick(1500);
+
+        expect(component.count).toEqual(1);
+      }))
+
+      it('setTimeout ausführen', () => {
+        component.countClick();
+        component.countClick();
+        expect(setTimeout).toHaveBeenCalledTimes(2);
+      })
+
+      it('test toDisplay boolean', () => {
+        component.clicked();
+        expect(component.toDisplay).toBe(true);
       })
     })
+  })
+
+  describe('Service: LoggingService', () => {
+    beforeEach(() => TestBed.configureTestingModule({
+      providers: [ LoggingService ]
+    }));
+
+    /*it('should return available languages', inject([LoggingService], service => {
+      expect(service.printLog());
+    }));*/
   })
 
   describe('Template', () => {
 
     it(`should have as title 'todos'`, () => {
-      expect(component.title).toBeTruthy();
-      //toEqual('todos')
+      expect(component.title).toEqual('todos');
     });
 
     it('should display message', () => {
       const messageElement = fixture.debugElement.query(By.css('.c-app__message')).nativeElement as HTMLElement;
-      expect(messageElement.textContent).toBe('The light is Off');
+      expect(messageElement.textContent).toBe('');
     });
   })
 
